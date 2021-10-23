d365d0835f8fc9f836834234b91a82558b68499e
Rinaldo Ramos <rinaldoramos87@gmail.com>
2021-10-23 00:47:33 -0400

    Second Commit
    Adding the Products Class and its variables/methods

---
+++ foodbox/src/main/java/com/simplilearn/workshop/foodbox/model/Products.java
@@ -0,0 +1,89 @@
+package com.simplilearn.workshop.foodbox.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Products implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+	private String productName;
+	private String brand;
+	private int available;
+	private double price;
+	private boolean status;
+	
+	public Products() {
+		super();
+	}
+
+	public Products(String productName, String brand, int available, double price, boolean status) {
+		super();
+		this.productName = productName;
+		this.brand = brand;
+		this.available = available;
+		this.price = price;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Products [id=" + id + ", productName=" + productName + ", brand=" + brand + ", available=" + available
+				+ ", price=" + price + ", status=" + status + "]";
+	}		
+}
